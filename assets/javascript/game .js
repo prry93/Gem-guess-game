b6feb49d9de7220cc8a20eacbf57dc6db7be8838
perry93 <prry93@gmail.com>
2019-05-24 23:47:15 -0700

    uploaded working vanilla js version

--- assets/javascript/game .js
+++ assets/javascript/game .js
@@ -4,104 +4,83 @@ var losses = 0;
 var totalScore = 0;
 var targetScore = 0;
 
+document.querySelector("#total-score").innerHTML = totalScore;
+
+document.querySelector("#numWins").innerHTML = wins;
+document.querySelector("#numLosses").innerHTML = wins;
+
+var gem1 = document.querySelector("#gem1");
+console.log(gem1);
+var gem2 = document.querySelector("#gem2");
+var gem3 = document.querySelector("#gem3");
+var gem4 = document.querySelector("#gem4");
+
 // generates the random value for the clickable crystal
 // var gem1= Math.floor(Math.random()*11)+1;
 
 function gemValue(gem) {
-    gem = Math.floor(Math.random()*11)+1;
+    var gem = Math.floor(Math.random()*6)+1;
     return gem
 }
 
-var gem1 = gemValue(gem1);
-var gem2 = gemValue(gem2);
-var gem3 = gemValue(gem3);
-var gem4 = gemValue(gem4);
-
-// Counts wins and losses
-$("#numWins").text(wins);
-$("#numLosses").text(losses);
+var gem1Value = gemValue(gem1Value);
+console.log(gem1Value);
+var gem2Value = gemValue(gem2Value);
+console.log(gem2Value);
+var gem3Value = gemValue(gem3Value);
+console.log(gem3Value);
+var gem4Value = gemValue(gem4Value);
+console.log(gem4Value);
 
 // will generate the random number that you are trying achieve
 var targetScore = Math.floor(Math.random()*102)+18;
+document.querySelector("#target-score").innerHTML = targetScore;
+console.log(targetScore);
 
-function newTargetScore(){
-    return targetScoreScore = Math.floor(Math.random()*102)+18;
+
+function gameLogic(gem) {
+    totalScore += gem;
+    $("#total-score").text(totalScore);
+    
+      if (totalScore === targetScore) {
+          wins++ ;
+          reset();
+      }
+      else if (totalScore > targetScore) {
+          losses ++;
+          reset();
+      }
 }
 
-newTargetScore(targetScore);
+gem1.addEventListener("click", function(){
+    gameLogic(gem1Value);
+})
+
+gem2.addEventListener("click", function(){
+    gameLogic(gem2Value);
+})
+
+gem3.addEventListener("click", function(){
+    gameLogic(gem3Value);
+})
+
+gem4.addEventListener("click", function(){
+    gameLogic(gem4Value);
+})
 
 // will reset game by setting the total user score to 0 and give new number values
 function reset(){
-  totalScore = 0;
-  targetScore = newTargetScore(targetScore);
-  console.log(targetScore);
-   gem1= gemValue(gem1);
-   gem2= gemValue(gem1);
-   gem3= gemValue(gem1);;
-   gem4= gemValue(gem1);
-  $("#numWins").text(wins);
-  $("#numLosses").text(losses);
-  $("#target-score").text(targetScore);
-  $("#total-score").text(totalScore);
-}
-
-// will display the score generated from targetScore
-$("#target-score").text(targetScore);
-
-// give click functionality and add to the total score
-
-// Note: wins and losses counter do not work
-
-$("#1").on("click", function(){
-    totalScore = totalScore + gem1;
-  $("#total-score").text(totalScore);
-    if (totalScore === targetScore) {
-        wins++ ;
-        reset();
-    }
-    else if (totalScore > targetScore) {
-        losses ++;
-        reset();
-    }
-});
-
-
-
-$("#2").on("click", function(){
-    totalScore = totalScore + gem2;
-  $("#total-score").text(totalScore);
-    if (totalScore === targetScore) {
-        wins++ ;
-        reset();
-    }
-    else if (totalScore > targetScore) {
-        losses ++;
-        reset();
-    }
-})
-
-$("#3").on("click", function(){
-    totalScore = totalScore + gem3;
-  $("#total-score").text(totalScore);
-    if (totalScore === targetScore) {
-        wins++ ;
-        reset();
-    }
-    else if (totalScore > targetScore) {
-        losses ++;   
-    }
-
-})
-
-$("#4").on("click", function(){
-    totalScore = totalScore + gem4;
-  $("#total-score").text(totalScore);
-    if (totalScore === targetScore) {
-        wins++ ;
-        reset();
-    }
-    else if (totalScore > targetScore) {
-        losses ++;
-        reset();
-    }
-})
+    totalScore = 0;
+    targetScore = Math.floor(Math.random()*102)+18;
+    console.log(targetScore);
+     gem1Value= gemValue(gem1Value);
+     gem2Value= gemValue(gem2Value);
+     gem3Value= gemValue(gem3Value);;
+     gem4Value= gemValue(gem4Value);
+     document.querySelector("#numWins").innerHTML = wins
+     console.log(wins);
+     document.querySelector("#numLosses").innerHTML = losses;
+     console.log(losses);
+    $("#target-score").text(targetScore);
+    document.querySelector("#total-score").innerHTML = totalScore;
+  }
